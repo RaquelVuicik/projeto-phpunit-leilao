ef7b4d49dc72db0e791885338af4058a22ac7796
Raquel Vuicik <raquelvuicik1@gmail.com>
2025-07-15 06:17:48 -0300

    teste automatizado

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,8 @@ use Raquel\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrange - Given
+// Arrumo a casa para o teste
 $leilao = new Leilao('Fiat 147 0km');
 
 $maria = new Usuario('Maria');
@@ -16,8 +18,19 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Act - When
+// Executo o código a ser testado
 $leiloeiro->avaliar($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+// Assert - Then
+// Verifico se a saída é a esperada
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+    echo "TESTE OK\n";
+} else {
+    echo "TESTE FALHOU\n";
+}
